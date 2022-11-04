6e751e1b4c3c117fc4229b9f7db4f51452e4ce98
Eric <eric200203@outlook.com>
2022-11-04 21:00:31 +0800

    🔨 Add Script to Convert Tiles into Base64 Strings.

---
+++ script/base64.js
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+
+(function() {
+    const dir = 'img';
+    const ext = '.png';
+    const dst = 'src/res.json';
+
+    const result = [];
+
+    const files = fs
+        .readdirSync(dir)
+        .filter((f) => path.extname(f) === ext);
+    for(const f of files) {
+        const bitmap = fs.readFileSync(path.join(dir, f));
+        const base64 = bitmap.toString('base64');
+        result.push({
+            k: path.basename(f, ext),
+            v: base64
+        });
+    }
+
+    fs.writeFileSync(dst, JSON.stringify(result), 'utf-8');
+})();
